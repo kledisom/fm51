98ea663689ff4af8908f33149c87ba8938465387
devkledisom <dev@ziiz.com.br>
2026-05-19 23:00:08 -0300

    feat: converter app para PWA com suporte offline

---
+++ sw.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'teste-ruptura-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index2.html',
+  './index.html',
+  './inicio-fim.html',
+  './teares.html',
+  './relatorio01.html',
+  './relatorio02.html',
+  './ocorrencia.html',
+  './rpm-artigo.html',
+  './index51.html',
+  './tecelagem.png',
+  './manifest.json',
+  './libs/chart.js/Chart.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((response) => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      }).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./index2.html');
+        }
+      });
+    })
+  );
+});
